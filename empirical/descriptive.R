2f39841c7fb32410671bf152a7c7cdb467b7e3b8
Sooyong Lee <sooyongl09@utexas.edu>
2024-02-16 12:22:55 -0600

    Update

--- empirical/descriptive.R
+++ empirical/descriptive.R
@@ -1,15 +1,18 @@
+library(flextable); library(officer)
 library(fs)
 library(glue)
 library(tidyverse)
 library(data.table)
 library(MplusAutomation)
 library(moments)
-
+source("empirical/table_source.R")
 select <- dplyr::select
 
-data <- fread("cleaneddata/cleaned.csv") %>% tibble()
+data <- fread("cleaneddata/empirical_data.csv") %>% tibble() #%>% 
+  # mutate(income = exp(income))
 
-psych::describe(data)
+data <- data %>%
+  filter_all(~ !is.na(.x))
 
 
 prop_ceiling <- apply(data[1:4], 2, function(x) {
@@ -22,8 +25,19 @@ prop_floor <- apply(data[1:4], 2, function(x) {
   round(sum(xx == min(xx, na.rm = T))/length(xx),3)
 })
 
-prop_ceiling
-prop_floor
+psych::describe(data) %>% 
+  slice(1:4) %>% 
+  select(mean, sd, skew, kurtosis) %>% 
+  mutate(
+    ceiling = prop_ceiling,
+    floor = prop_floor
+  ) %>% 
+  mutate_all(round, 3) %>% 
+  mutate(vars = c("y1","y2","y3","y4"),
+         .before = mean, 
+         ) %>% 
+  mk_tbl("Table. Desci")
+
 
 long_data <- data %>% 
   select(y1:y4) %>% 
@@ -40,11 +54,15 @@ long_data %>%
   labs(x = "", y = "Freq") +
   cowplot::theme_cowplot(font_size = 14)
 
+ggsave("results/emp_censprop.png", height = 4, width = 6)
+
 long_data %>% 
   filter(
-    id %in% sample(1:dim(data)[1], 1000)
+    id %in% sample(1:dim(data)[1], 500)
   ) %>% 
   ggplot(aes(y = value, x = time)) +
   geom_line(aes(group = id), alpha = 0.1) +
   labs(x = "Time", y = "Expectation") +
   cowplot::theme_cowplot(font_size = 14)
+
+ggsave("results/emp_lines.png", height = 4, width = 6)
