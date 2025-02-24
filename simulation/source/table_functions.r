1b57f22bff997717a8b9707e10692980e6d53e7c
Sooyong Lee <sooyongl09@utexas.edu>
2025-02-24 09:39:06 -0600

    Update table_functions.r

--- simulation/source/table_functions.r
+++ simulation/source/table_functions.r
@@ -31,48 +31,48 @@ table.phase2 <- function(ft_data, filter.inp = "I~1", caption="", x.width = 1) {
   # x.width = 1
   # caption = "Table 4-2. Intercept"
   
-  ft_data <- ft_data %>% 
-    select(nobs, ntimepoint, cenprop_chr, ICC, path,
-           starts_with("rbias"), starts_with("mse")) %>% 
-    
-    filter(path == filter.inp) %>% 
-    
-    select(nobs, ntimepoint, cenprop_chr, ICC,
-           starts_with("rbias"), starts_with("mse")) %>% 
-    # filter(cenprop_chr == cenprop.val) %>% 
-    
-    # dplyr::group_by(nobs, ntimepoint, cenprop_chr) %>%
-    # dplyr::summarise(n = dplyr::n(), .groups = "drop") %>%
-    # dplyr::filter(n > 1L)
-    
-    pivot_wider(names_from = "cenprop_chr", 
-                values_from = c("rbias_cen", "rbias_gbit", 
-                                "mse_cen", "mse_gbit", "mse_ratio")
-    ) %>% 
-    # mutate(
-    #   growthvar_chr =
-    #     case_when(
-    #       growthvar_chr == "1_0.5_0" ~ "0",
-    #       TRUE ~ "0.2"
-    #     )
+  ft_data <- ft_data  %>%  mutate_if(is.numeric, round, 4)
+    # select(nobs, ntimepoint, cenprop_chr, ICC, path,
+    #        starts_with("rbias"), starts_with("mse")) %>% 
+    # 
+    # # filter(path == filter.inp) %>% 
+    # 
+    # select(nobs, ntimepoint, cenprop_chr, ICC,
+    #        starts_with("rbias"), starts_with("mse")) %>% 
+    # # filter(cenprop_chr == cenprop.val) %>% 
+    # 
+    # # dplyr::group_by(nobs, ntimepoint, cenprop_chr) %>%
+    # # dplyr::summarise(n = dplyr::n(), .groups = "drop") %>%
+    # # dplyr::filter(n > 1L)
+    # 
+    # pivot_wider(names_from = "cenprop_chr", 
+    #             values_from = c("rbias_cen", "rbias_gbit", 
+    #                             "mse_cen", "mse_gbit", "mse_ratio")
     # ) %>% 
-    select(-matches("mse_cen|mse_gbit")) %>% 
-    select(nobs, ntimepoint, ICC,
-           # matches("^bias"),
-           # matches("^mse_"),
-           # matches("^mse_ratio")
-           matches("0.05-0.95"),
-           matches("0.1-0.9"),
-           matches("0.2-0.8"),
-           matches("0.3-0.7"),
-           matches("0.4-0.6")
-    )
+    # # mutate(
+    # #   growthvar_chr =
+    # #     case_when(
+    # #       growthvar_chr == "1_0.5_0" ~ "0",
+    # #       TRUE ~ "0.2"
+    # #     )
+    # # ) %>% 
+    # select(-matches("mse_cen|mse_gbit")) %>% 
+    # select(nobs, ntimepoint, ICC,
+    #        # matches("^bias"),
+    #        # matches("^mse_"),
+    #        # matches("^mse_ratio")
+    #        matches("0.05-0.95"),
+    #        matches("0.1-0.9"),
+    #        matches("0.2-0.8"),
+    #        matches("0.3-0.7"),
+    #        matches("0.4-0.6")
+    # )
   
   # length(4:dim(ft_data)[2])
   # Header 1
   header1 <- c()
-  header1[1:3] <- c("")
-  header1[4:dim(ft_data)[2]] <- 
+  header1[1:2] <- c("")
+  header1[3:dim(ft_data)[2]] <- 
     rep(c("0.05-0.95",
           "0.1-0.9",
           "0.2-0.8",
@@ -81,16 +81,16 @@ table.phase2 <- function(ft_data, filter.inp = "I~1", caption="", x.width = 1) {
   
   # Header 2
   header2 <- c()
-  header2[1:3] <- c("")
-  header2[4:dim(ft_data)[2]] <- 
+  header2[1:2] <- c("")
+  header2[3:dim(ft_data)[2]] <- 
     # rep(c(rep(c("Bias","MSE"), each = 2),"MSER"), 5)
-    rep(c(rep(c("rBias"), each = 2),"MSER"), 5)
+    rep(c(rep(c("rBias"), each = 2),"Diff"), 5)
   # length(header2)
   
   # Header 3
   header3 <- c()
-  header3[1:3] <- c("N", "TP","ICC")
-  header3[4:dim(ft_data)[2]] <- 
+  header3[1:2] <- c("Path","N")
+  header3[3:dim(ft_data)[2]] <- 
     # rep(c(rep(c("Cen","GBIT"), 2),"."), 5)
     rep(c(rep(c("cML","GBIT"), 1),"."), 5)
     
@@ -102,9 +102,9 @@ table.phase2 <- function(ft_data, filter.inp = "I~1", caption="", x.width = 1) {
     header3 = header3,
     stringsAsFactors = FALSE )
   
-  mser_pos <- str_which(names(ft_data), "mse_ratio")
+  mser_pos <- str_which(names(ft_data), "sd_diff")
   temp_dt  <- ft_data %>% 
-    select(contains("mse_ratio"))
+    select(contains("sd_diff"))
   
   coloring <- function(x, coln, temp_dt, mser_pos) {
     bg(x,
@@ -129,6 +129,8 @@ table.phase2 <- function(ft_data, filter.inp = "I~1", caption="", x.width = 1) {
   ft_1 <- merge_h(ft_1, i = 2, part = "header")
   
   ft_1 %>% 
+    set_caption(caption = caption) %>%
+    add_footer_lines(values = "Note. N: sample size; TP: the number of timepoints; GBIT: generalized tobit estimator; cML: ML estimator with censored data") %>%
     padding(., padding.top = 0.1, padding.bottom = 0.1, 
             padding.left=0,padding.right=0, part = "header") %>% 
     padding(., padding.top = 0.1, padding.bottom = 0.1, 
@@ -147,12 +149,12 @@ table.phase2 <- function(ft_data, filter.inp = "I~1", caption="", x.width = 1) {
     align(align = "right", part = "body") %>% 
     flextable::border(.,
            border.right = fp_border(color = "black", width = .75),
-           j =c(3,6,9,12,15)
+           j =c(2,5,8,11,14)
            
     ) %>% 
     flextable::border(.,
            border.bottom = fp_border(color = "black", width = .75),
-           i =c(6, 12, 18)
+           i =c(4, 8, 12)
            
     ) %>% 
     flextable::border(.,
@@ -160,24 +162,23 @@ table.phase2 <- function(ft_data, filter.inp = "I~1", caption="", x.width = 1) {
                       i = inline_row
                       
     ) %>% 
-    merge_v(j = c(1:3)) %>% 
+    merge_v(j = c(1:2)) %>% 
   
-    width(j = 4:18, width = x.width) %>%
-    width(j = 1:3, width = 0.5) %>%
-    set_caption(caption = caption) %>%
-    add_footer_lines(values = "Note. N: sample size; TP: the number of timepoints; GBIT: generalized tobit estimator; cML: ML estimator with censored data") %>%
+    width(j = 3:12, width = x.width) %>%
+    width(j = 1:2, width = 0.5) %>%
+
     
-    coloring(1, temp_dt, mser_pos) %>% 
-    coloring(2, temp_dt, mser_pos) %>% 
-    coloring(3, temp_dt, mser_pos) %>% 
-    coloring(4, temp_dt, mser_pos) %>% 
-    coloring(5, temp_dt, mser_pos) %>% 
-    
-    bolding(1, temp_dt, mser_pos) %>% 
-    bolding(2, temp_dt, mser_pos) %>% 
-    bolding(3, temp_dt, mser_pos) %>% 
-    bolding(4, temp_dt, mser_pos) %>% 
-    bolding(5, temp_dt, mser_pos) %>% 
+    # coloring(1, temp_dt, mser_pos) %>% 
+    # coloring(2, temp_dt, mser_pos) %>% 
+    # coloring(3, temp_dt, mser_pos) %>% 
+    # coloring(4, temp_dt, mser_pos) %>% 
+    # coloring(5, temp_dt, mser_pos) %>% 
+    # 
+    # bolding(1, temp_dt, mser_pos) %>% 
+    # bolding(2, temp_dt, mser_pos) %>% 
+    # bolding(3, temp_dt, mser_pos) %>% 
+    # bolding(4, temp_dt, mser_pos) %>% 
+    # bolding(5, temp_dt, mser_pos) %>% 
     
     fix_border_issues()
   
